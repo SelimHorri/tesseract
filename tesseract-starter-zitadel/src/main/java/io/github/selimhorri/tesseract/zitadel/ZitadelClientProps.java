5fe31cc937e60b942e544f95e5280a60ac9bf75f
s.horri <S.HORRI@oodrive.com>
2026-04-06 15:13:52 +0100

    Add Docker Compose setup for Zitadel and enhance `ZitadelClientProps` with validation annotations and additional properties

--- tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/ZitadelClientProps.java
+++ tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/ZitadelClientProps.java
@@ -1,5 +1,6 @@
 package io.github.selimhorri.tesseract.zitadel;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
@@ -9,10 +10,15 @@ import java.util.Set;
 
 @ConfigurationProperties(prefix = "tesseract.zitadel")
 @Validated
-public record ZitadelClientProps(@DefaultValue("http://127.0.0.1:8080") String baseUrl, Set<String> publicUris) {
+public record ZitadelClientProps(@DefaultValue("http://127.0.0.1:8080") String baseUrl,
+								 @NotBlank(message = "client-id MUST be specified") String clientId,
+								 @NotBlank(message = "client-secret MUST be specified") String clientSecret,
+								 @DefaultValue("client_credentials") String grantType,
+								 @DefaultValue("openid") String scope,
+								 Set<String> whitelistedPaths) {
 	
 	public ZitadelClientProps {
-		publicUris = Set.copyOf(Objects.requireNonNullElseGet(publicUris, Set::of));
+		whitelistedPaths = Set.copyOf(Objects.requireNonNullElseGet(whitelistedPaths, Set::of));
 	}
 	
 }
