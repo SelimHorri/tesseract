072dc678290ec17d34672897b38a50851dd1f051
s.horri <S.HORRI@oodrive.com>
2026-04-07 09:30:13 +0100

    Update OAuth endpoint paths in HTTP client interfaces

--- tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/TokenRetrieverAsyncHttpClient.java
+++ tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/TokenRetrieverAsyncHttpClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 import reactor.core.publisher.Mono;
 
-@HttpExchange("/v2/oauth")
+@HttpExchange("/oauth/v2")
 public interface TokenRetrieverAsyncHttpClient {
 	
 	@PostExchange(url = "/token", accept = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
--- tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/TokenRetrieverSyncHttpClient.java
+++ tesseract-starter-zitadel/src/main/java/io/github/selimhorri/tesseract/zitadel/TokenRetrieverSyncHttpClient.java
@@ -4,7 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
-@HttpExchange("/v2/oauth")
+@HttpExchange("/oauth/v2")
 public interface TokenRetrieverSyncHttpClient {
 	
 	@PostExchange(url = "/token", accept = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
